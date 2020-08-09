217e3a8c05231e6e9e0facb1ddeb2dd7b4aab632
Milton Bravo <miltonbravo96@gmail.com>
2020-08-08 23:05:52 -0500

    Cadena de conexión actualizada

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { MONGODB_HOST, MONGODB_USER, MONGODB_PASSWORD, MONGODB_DATABASE } = process.env;
-const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}.vxrsx.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,

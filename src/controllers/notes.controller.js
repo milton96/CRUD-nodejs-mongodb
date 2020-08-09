6021727cf1e73dabf9ddaa9fba15782925ac4d39
Milton Bravo <miltonbravo96@gmail.com>
2020-08-09 16:53:49 -0500

    lista de todas las notas

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,16 +1,22 @@
 const notesController = {};
+const Note = require('../models/Note');
 
-notesController.renderNotes = (req, res) => {
-    res.send('notes');
+notesController.renderNotes = async (req, res) => {
+    const notes = await Note.find();
+    res.render('notes/all-notes', { notes });
 };
 
 notesController.renderNoteForm = (req, res) => {
     res.render('notes/new-note');
 };
 
-notesController.createNewNote = (req, res) => {
-    res.send('create new note');
-    console.log(req.body);
+notesController.createNewNote = async (req, res) => {
+    const { title, description } = req.body;
+    let note = new Note({
+        title, description
+    });
+    await note.save();
+    res.status(200).send('Ok');
 };
 
 notesController.renderEditNote = (req, res) => {
